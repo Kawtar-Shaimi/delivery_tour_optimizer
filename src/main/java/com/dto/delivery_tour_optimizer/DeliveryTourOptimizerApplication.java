0486819c7f4bc0c6fa11677e7b65f6ce84c381b9
Kawtar <Kawtar.shaimi8@gmail.com>
2025-10-26 13:48:37 +0100

    Initialisation du projet.

---
+++ src/main/java/com/dto/delivery_tour_optimizer/DeliveryTourOptimizerApplication.java
@@ -0,0 +1,13 @@
+package com.dto.delivery_tour_optimizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryTourOptimizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryTourOptimizerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dto/delivery_tour_optimizer/DeliveryTourOptimizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dto.delivery_tour_optimizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryTourOptimizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
