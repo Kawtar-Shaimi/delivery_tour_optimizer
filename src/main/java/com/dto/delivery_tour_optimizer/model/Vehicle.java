2670159fa0b7eeaacb4d39be05479ee2e5d0fb20
Kawtar <Kawtar.shaimi8@gmail.com>
2025-10-26 16:02:39 +0100

    DT-6 : AAjout de model Tour.

--- src/main/java/com/dto/delivery_tour_optimizer/model/Vehicle.java
+++ src/main/java/com/dto/delivery_tour_optimizer/model/Vehicle.java
@@ -1,4 +1,31 @@
 package com.dto.delivery_tour_optimizer.model;
 
+import com.dto.delivery_tour_optimizer.model.enums.VehicleType;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.Id;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Vehicle {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private VehicleType type;
+
+    private String licensePlate;
+    private double maxWeight;    // en kg
+    private double maxVolume;    // en m³
+    private int maxDeliveries;   // nombre max de livraisons
+
+    // Méthode utilitaire pour vérifier les contraintes
+    public boolean canCarry(double weight, double volume) {
+        return weight <= maxWeight && volume <= maxVolume;
+    }
 }
