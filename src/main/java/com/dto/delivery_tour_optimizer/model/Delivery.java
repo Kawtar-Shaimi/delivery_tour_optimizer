1d21e78c2b5b7156feeadb0d2b9f487a20720660
Kawtar <Kawtar.shaimi8@gmail.com>
2025-10-26 15:03:17 +0100

    DT-6 : Ajout de Delivery model.

--- src/main/java/com/dto/delivery_tour_optimizer/model/Delivery.java
+++ src/main/java/com/dto/delivery_tour_optimizer/model/Delivery.java
@@ -1,4 +1,30 @@
 package com.dto.delivery_tour_optimizer.model;
 
+import com.dto.delivery_tour_optimizer.model.enums.DeliveryStatus;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Delivery {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String address;
+    private double latitude;
+    private double longitude;
+    private double weight;
+    private double volume;
+    private String timeSlot; // ex : 09:00-11:00
+
+    @Enumerated(EnumType.STRING)
+    private DeliveryStatus status;
+
+    @ManyToOne
+    private Tour tour;
 }
