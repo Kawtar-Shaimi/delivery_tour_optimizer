a1db25584c495a2152fd312bdedbc6a2c5dc4423
Kawtar <Kawtar.shaimi8@gmail.com>
2025-10-26 18:05:29 +0100

    DTO-5 : Création de Tour Optimizer interface

--- src/main/java/com/dto/delivery_tour_optimizer/service/TourOptimizer.java
+++ src/main/java/com/dto/delivery_tour_optimizer/service/TourOptimizer.java
@@ -1,4 +1,13 @@
 package com.dto.delivery_tour_optimizer.service;
 
-public class TourOptimizer {
+import com.dto.delivery_tour_optimizer.model.Delivery;
+import com.dto.delivery_tour_optimizer.model.Vehicle;
+import com.dto.delivery_tour_optimizer.model.Warehouse;
+import java.util.List;
+
+public interface TourOptimizer {
+    List<Delivery> calculateOptimalTour(List<Delivery> deliveries, Warehouse warehouse, Vehicle vehicle);
+
+    // Méthode utilitaire pour calculer la distance totale
+    double calculateTotalDistance(List<Delivery> route, Warehouse warehouse);
 }
