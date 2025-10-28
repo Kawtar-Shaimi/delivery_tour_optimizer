ac5c61f6f3c72cbaf5aead853524e56c206939cf
Kawtar <Kawtar.shaimi8@gmail.com>
2025-10-28 11:02:49 +0100

    DTO-7 : Ajout de fichier .env

--- src/main/java/com/dto/delivery_tour_optimizer/DeliveryTourOptimizerApplication.java
+++ src/main/java/com/dto/delivery_tour_optimizer/DeliveryTourOptimizerApplication.java
@@ -1,13 +1,22 @@
 package com.dto.delivery_tour_optimizer;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DeliveryTourOptimizerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DeliveryTourOptimizerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        loadEnv();
+        SpringApplication.run(DeliveryTourOptimizerApplication.class, args);
+    }
+
+    private static void loadEnv(){
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+        dotenv.entries().forEach(entry -> {
+            System.setProperty(entry.getKey(), entry.getValue());
+        });
+    }
 
 }
